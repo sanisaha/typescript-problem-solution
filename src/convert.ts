5c876832c0dc796402ab89c1acdbfee3e713a6c6
sanisaha <44751151+sanisaha@users.noreply.github.com>
2023-07-01 01:11:49 +0300

    first two problem solved

---
+++ src/convert.ts
@@ -0,0 +1,20 @@
+// Convert the following JavaScript array into a TypeScript tuple
+// const userInfo = [101, "Ema", "John", true,  , "2023"];
+
+const userInfo: [number, string] = [101, 'Ema'];
+const userInfo2: [string, boolean] = ['John', true];
+const userInfo3: [undefined, string] = [, '2023'];
+
+// Write a TypeScript function that takes in two arrays of numbers as parameters. The function should compare the elements in both arrays and return a new array that contains only the elements that are present in both arrays.
+
+const firstArray: number[] = [1, 2, 3, 4, 5]
+const secondArray: number[] = [2, 4, 6, 8]
+
+
+function getCommonArray(array1: number[], array2: number[]): number[] {
+    const newArray = array1.filter((value: number) => array2.includes(value))
+    return newArray
+
+}
+
+getCommonArray(firstArray, secondArray)
